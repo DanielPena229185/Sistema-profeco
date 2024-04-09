7fcf389a050971a192bc73ad04c6586bd9aa49a2
DanielPenaGarcia <daniel.pena229185@potros.itson.edu.mx>
2024-04-08 21:30:47 -0700

    Errores corregidos al cargar los precios
    Además se agrego servicio de notificaciones para los toast

--- app-cliente-movil/src/app/components/comparacion-producto-tiendas/comparacion-producto-tiendas.component.ts
+++ app-cliente-movil/src/app/components/comparacion-producto-tiendas/comparacion-producto-tiendas.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {
+  PriceDTO,
   ProductDTO,
+  ProductInfoDTO,
   ProductoDTO,
 } from './comparacion-producto-tiendas.component.types';
 import {
@@ -19,19 +21,20 @@ import {
   IonThumbnail,
   IonTitle,
   IonToolbar,
-  ModalController,
-} from '@ionic/angular/standalone';
+  ModalController, IonSkeletonText, IonListHeader } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 import { DineroFormatoPipe } from './pipes/dinero-formato/dinero-formato.pipe';
 import { ComparacionProductosTiendasService } from './comparacion-productos-tiendas.service';
 import { HttpClientModule } from '@angular/common/http';
+import { LoadingController } from '@ionic/angular';
+import { NotificationService } from 'src/app/shared/services/notification.service';
 
 @Component({
   selector: 'app-comparacion-producto-tiendas',
   templateUrl: './comparacion-producto-tiendas.component.html',
   styleUrls: ['./comparacion-producto-tiendas.component.scss'],
   standalone: true,
-  imports: [
+  imports: [IonListHeader, IonSkeletonText, 
     IonCard,
     IonCardHeader,
     IonCardContent,
@@ -57,21 +60,47 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class ComparacionProductoTiendasComponent implements OnInit {
   productoId: string;
-  producto: ProductoDTO;
   product: ProductDTO;
+  prices: PriceDTO[];
 
   constructor(
-    private modalCtrl: ModalController,
-    private readonly comparacionProductoTiendas: ComparacionProductosTiendasService
+    private readonly modalCtrl: ModalController,
+    private readonly comparacionProductoTiendas: ComparacionProductosTiendasService,
+    private readonly loadingCtrl: LoadingController,
+    private readonly notificationService: NotificationService
   ) {}
 
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.getProductById(this.productoId);
+  }
+  
   closeModal() {
     this.modalCtrl.dismiss();
   }
 
+  async showLoading(): Promise<any>{
+    const loading = await this.loadingCtrl.create({
+      message: 'Cargando precios',
+      cssClass: 'spinner',
+      translucent: true
+    });
+    loading.present();
+    return loading;
+  }
+
   getProductById(productId: string) {
-    
+    this.showLoading().then((loading) => {
+      this.comparacionProductoTiendas.getProductInfoByProductId(productId).subscribe({
+        next: ((productInfo: ProductInfoDTO) => {
+          this.product = productInfo.product;
+          this.prices = productInfo.prices;
+          loading.dismiss();
+        }),
+        error: ((error) => {
+          this.notificationService.presentToast('Ocurrio un error al cargar los precios', 'danger')
+          loading.dismiss();
+        })
+      })
+    })
   }
 }
--- app-cliente-movil/src/app/components/comparacion-producto-tiendas/comparacion-productos-tiendas.service.ts
+++ app-cliente-movil/src/app/components/comparacion-producto-tiendas/comparacion-productos-tiendas.service.ts
@@ -2,17 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ProductInfoDTO } from './comparacion-producto-tiendas.component.types';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ComparacionProductosTiendasService {
+  constructor(private readonly http: HttpClient) {}
 
-  constructor(
-    private readonly http: HttpClient
-  ) { }
-
-  getProductInfoByProductId(productId: string){
-    return this.http.get<ProductInfoDTO>(`${environment.apiURL}/prices`, )
+  getProductInfoByProductId(productId: string): Observable<ProductInfoDTO> {
+    return this.http.get<ProductInfoDTO>(
+      `${environment.apiURL}/products/prices/${productId}`
+    );
   }
 }
