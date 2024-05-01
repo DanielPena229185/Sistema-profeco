da2805f1bb56b99071b3d49a30d301afbfeb12d5
DanielPenaGarcia <daniel.pena229185@potros.itson.edu.mx>
2024-05-01 12:19:00 -0700

    X

--- MarketMicroservice/obj/Debug/net8.0/Market.AssemblyInfo.cs
+++ MarketMicroservice/obj/Debug/net8.0/Market.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Market")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7582664889739a023626f037f0b9455c56d4146f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be9590f8ad12f954a856d1fb6123355c4369a03a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Market")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Market")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
