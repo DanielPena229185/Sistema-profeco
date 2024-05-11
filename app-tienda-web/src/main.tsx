b45ee1ace41a08d11a11dcf951a2f29ea739c881
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-05-11 12:21:46 -0700

    Update routes

--- app-tienda-web/src/main.tsx
+++ app-tienda-web/src/main.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App/App';
+import 'material-icons/iconfont/material-icons.css';
+import Root from './routes/Root';
+import ErrorPage from './ErrorPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Products from './components/Products/Products';
+import Reports from './components/Reports/Reports';
+import Deals from './components/Deals/Deals';
+import Product from './components/Products/Product/Product';
+import Comments from './components/Comments/Comments';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'productos',
+        element: <Products />,
+      },
+      {
+        path: 'productos/:id',
+        element: <Product />,
+      },
+      {
+        path: 'reportes',
+        element: <Reports />,
+      },
+      {
+        path: 'ofertas',
+        element: <Deals />,
+      },
+      {
+        path: 'comentarios',
+        element: <Comments />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
