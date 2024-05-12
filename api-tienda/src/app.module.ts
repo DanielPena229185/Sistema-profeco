7a71bf7c2db3b20b9814620311b3aea519fa6544
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-05-11 22:00:02 -0700

    Update imports

--- api-tienda/src/app.module.ts
+++ api-tienda/src/app.module.ts
@@ -4,10 +4,30 @@ import { AppService } from './app.service';
 import { MarketController } from './market/market.controller';
 import { MarketService } from './market/market.service';
 import { ClientModules } from './microservices/microservices';
+import { ReportsController } from './reports/reports.controller';
+import { ProductsController } from './products/products.controller';
+import { ProductsService } from './products/products.service';
+import { ReportsService } from './reports/reports.service';
+import { DealService } from './deal/deal.service';
+import { DealController } from './deal/deal.controller';
+import { ReviewsService } from './reviews/reviews.service';
 
 @Module({
   imports: [ClientModules],
-  controllers: [AppController, MarketController],
-  providers: [AppService, MarketService],
+  controllers: [
+    AppController,
+    MarketController,
+    ReportsController,
+    ProductsController,
+    DealController,
+  ],
+  providers: [
+    AppService,
+    MarketService,
+    ReportsService,
+    ProductsService,
+    DealService,
+    ReviewsService,
+  ],
 })
 export class AppModule {}
