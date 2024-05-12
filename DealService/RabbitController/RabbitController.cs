6a5a12c57e45b65787b1f5f3fe2dd374e986916e
DanielPenaGarcia <daniel.pena229185@potros.itson.edu.mx>
2024-05-11 18:00:32 -0700

    Ya no truena al mandar el mensaje al rabbit

--- DealService/RabbitController/RabbitController.cs
+++ DealService/RabbitController/RabbitController.cs
@@ -13,20 +13,16 @@ public class RabbitController
     {
         _factory = new ConnectionFactory { HostName = "localhost" };
         _connection = _factory.CreateConnection();
-        _channel = _connection.CreateModel();
     }
 
     public static void SendMessage(string message, string exchange)
     {
-        if (_instance == null)
-        {
-            _instance = new RabbitController();
-        }
+        _instance = new RabbitController();
+        _channel = _connection.CreateModel();
         var body = Encoding.UTF8.GetBytes(message);
         _channel.BasicPublish(exchange: exchange,
                             routingKey: string.Empty,
                             basicProperties: null,
                             body: body);
-        Console.WriteLine($" [x] Sent {message}");
     }
 }
