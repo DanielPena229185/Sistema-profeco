1b213000113a49c5c67b91df4220665d87d1dfc7
silverflin <toledorusso@outlook.com>
2024-05-12 20:06:25 -0700

    chore: update urls

--- profeco-report-system-service/src/configs/rabbit.config.ts
+++ profeco-report-system-service/src/configs/rabbit.config.ts
@@ -1,11 +1,14 @@
 import { RabbitMQModule } from "@golevelup/nestjs-rabbitmq";
 
-export const RabbitConfig = RabbitMQModule.forRoot(RabbitMQModule,{
-    exchanges:[
+export const RabbitConfig = RabbitMQModule.forRoot(RabbitMQModule, {
+    exchanges: [
         {
             name: 'notifier',
             type: 'topic'
         }
     ],
-    uri: 'amqp://localhost:5672'
+    // add .dotenv file
+    // Ahorita estara configurado para produccion
+    // uri: 'amqp://localhost:5672'
+    uri: 'amqp://rabbitmq:5672'
 });
