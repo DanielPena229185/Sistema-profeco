a40c6cc2c4ddd7966e4addc1eb1aba33b103479f
Edgar Emir Borbón Jimenez <edemboji@gmail.com>
2024-05-15 18:23:38 -0700

    wishlist page

---
+++ app-cliente-movil/src/app/components/market-preference/market-preference.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { MarketPreferenceService } from './market-preference.service';
+import { MarketDTO } from './market-preference.types';
+@Component({
+  selector: 'app-market-preference',
+  templateUrl: './market-preference.component.html',
+  styleUrls: ['./market-preference.component.scss'],
+  standalone: true,
+  imports: [IonicModule,CommonModule,HttpClientModule],
+  providers:[MarketPreferenceService]
+})
+export class MarketPreferenceComponent  implements OnInit {
+  markets: MarketDTO[] = [];
+  constructor(
+    private readonly marketPreferenceService:MarketPreferenceService,
+  ) {}
+
+  ngOnInit() {
+    this.initMarkets();
+    //this.getMarketsPrefered();
+  }
+
+  initMarkets(){
+    this.markets=[
+      {
+        id:'001',
+        address: "Av. Springfiled",
+        name:'Walmart',
+        urlImg:'https://thelogisticsworld.com/wp-content/uploads/2023/04/walmart-2.jpg'
+      },
+      {
+        id:'002',
+        address: "Av. Springfiled",
+        name:'Bodega Aurrera',
+        urlImg:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyOH5IyWyQplolLNLpfRuq_umkUTOp986vRcVGBv9sTA&s'
+      },
+      {
+        id:'003',
+        address: "Av. Springfiled",
+        name:'Costco',
+        urlImg:'https://tb-static.uber.com/prod/image-proc/processed_images/096dc71e8c35944dbaec37ce43b0ea53/c9252e6c6cd289c588c3381bc77b1dfc.jpeg'
+      }
+    ]
+  }
+
+  getMarketsPrefered() {
+    this.marketPreferenceService.getMarketsPreference().subscribe({
+      next: (response: MarketDTO[]) => {
+        Array.prototype.push.apply(this.markets, response);
+      }
+    })
+  }
+
+}
--- app-cliente-movil/src/app/wishlist/wishlist.page.ts
+++ app-cliente-movil/src/app/wishlist/wishlist.page.ts
@@ -1,20 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 import { HeaderComponent } from '../components/header/header.component';
+import { FormsModule } from '@angular/forms';
+import { ProductsPreferenceComponent } from '../components/products-preference/products-preference.component';
+import { MarketPreferenceComponent } from '../components/market-preference/market-preference.component';
 
 @Component({
   selector: 'app-wishlist',
   templateUrl: './wishlist.page.html',
   styleUrls: ['./wishlist.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, HeaderComponent]
+  imports: [
+    IonicModule,
+    FormsModule, 
+    CommonModule, 
+    HeaderComponent,
+    ProductsPreferenceComponent,
+    MarketPreferenceComponent]
 })
 export class WishlistPage implements OnInit {
 
+  option:string = "productos";
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleRefresh(event) {
+    setTimeout(() => {
+      //this.getProducts();
+      event.target.complete();
+    }, 2000);
   }
 
 }
