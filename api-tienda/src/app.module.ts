c28fc402f0e4d7e582440d06478a26b546276f72
silverflin <toledorusso@outlook.com>
2024-05-16 21:57:01 -0700

    feat: auth middleware api-tienda

--- api-tienda/src/app.module.ts
+++ api-tienda/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { MarketController } from './market/market.controller';
 import { MarketService } from './market/market.service';
 import { ClientModules } from './microservices/microservices';
@@ -10,22 +10,30 @@ import { DealService } from './deal/deal.service';
 import { DealController } from './deal/deal.controller';
 import { ReviewsService } from './reviews/reviews.service';
 import { ReviewsController } from './reviews/reviews.controller';
+import { AuthMiddleware } from './common/middleware/auth.middleware';
 
 @Module({
-  imports: [ClientModules],
-  controllers: [
-    MarketController,
-    ReportsController,
-    ProductsController,
-    DealController,
-    ReviewsController,
-  ],
-  providers: [
-    MarketService,
-    ReportsService,
-    ProductsService,
-    DealService,
-    ReviewsService,
-  ],
+    imports: [ClientModules],
+    controllers: [
+        MarketController,
+        ReportsController,
+        ProductsController,
+        DealController,
+        ReviewsController,
+    ],
+    providers: [
+        MarketService,
+        ReportsService,
+        ProductsService,
+        DealService,
+        ReviewsService,
+    ],
 })
-export class AppModule {}
+
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(AuthMiddleware)
+            .forRoutes({ path: '*', method: RequestMethod.ALL });
+    }
+}
