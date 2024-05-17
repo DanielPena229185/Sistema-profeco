b0ce6f8a3bb2bd919ea9494d012b8b39bed743da
Edgar Emir Borbón Jimenez <edemboji@gmail.com>
2024-05-17 01:43:37 -0700

    rabbit

--- app-cliente-movil/src/app/components/header/header.component.ts
+++ app-cliente-movil/src/app/components/header/header.component.ts
@@ -6,6 +6,7 @@ import { notificationsOutline} from 'ionicons/icons';
 import { NotificationsListComponent } from '../notifications-list/notifications-list.component';
 import { DealDTO } from './header.types';
 import { Router } from '@angular/router';
+import { Client } from '@stomp/stompjs';
 
 @Component({
   selector: 'app-header',
@@ -15,14 +16,25 @@ import { Router } from '@angular/router';
   imports: [IonicModule],
 })
 export class HeaderComponent implements OnInit {
-  private dealsList:DealDTO[] =[];
+  private client:Client;
   constructor(
     private readonly router: Router, 
   ) {
     addIcons({notificationsOutline});
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.client = new Client({
+      brokerURL: 'ws://localhost:15674/ws',
+      onConnect:()=>{
+        this.client.subscribe('/topic/reports-queue',(message)=>{
+          console.log(`Recieved ${message.body}`)
+        });
+        this.client.publish({destination:'/topic/reports-queue',body:JSON.stringify('Message')});
+      }
+    });
+    this.client.activate();
+  }
 
   async goToNotifications(){
     this.router.navigate(["/posts"]);
