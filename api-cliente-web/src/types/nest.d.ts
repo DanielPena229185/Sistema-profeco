5c61b99ea41b31b1f3c8ac9110a08a267175abea
silverflin <toledorusso@outlook.com>
2024-05-16 21:11:25 -0700

    feat: auth middleware

--- api-cliente-web/src/types/nest.d.ts
+++ api-cliente-web/src/types/nest.d.ts
@@ -2,11 +2,13 @@
 interface User {
     id: string;
     email: string;
+    curp?: string;
+    nombres?: string;
+    apellidos?: string;
 }
 
 declare namespace Express {
     export interface Request {
         user?: User;
-
     }
 }
